34cf1166a3813c321620660074756ea5b2a42e95
Evin <evinfrancis0602@gmail.com>
2026-02-09 22:02:09 +0530

    2nd app

--- Webapp/urls.py
+++ Webapp/urls.py
@@ -2,4 +2,6 @@ from django.urls import path
 from Webapp import views
 urlpatterns=[
     path('home/',views.home,name='home'),
+    path('products/',views.products,name='products'),
+    path('about/',views.about,name='about'),
 ]
--- Webapp/views.py
+++ Webapp/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
+from Adminapp.models import *
 
 # Create your views here.
 def home(request):
-    return render(request,'index.html')
+    Category=CategoryDb.objects.all()
+    return render(request,'index.html' ,{"category":Category})
+
+
+def about(request):
+    return render(request,'about.html')
+def products(request):
+    Category=CategoryDb.objects.all()
+    products=ProductDb.objects.all()
+    return render(request,"products.html",
+                  {"category":Category,
+                  "products":products}
+                  )
